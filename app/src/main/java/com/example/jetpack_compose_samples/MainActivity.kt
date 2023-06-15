309bc40433795304e77f6a420c3b11a61f4081f6
BishwajithBN <46152209+BishwajithBN@users.noreply.github.com>
2023-06-15 16:49:50 +0530

    Row and colum added. implemented theme

--- app/src/main/java/com/example/jetpack_compose_samples/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_samples/MainActivity.kt
@@ -3,41 +3,71 @@ package com.example.jetpack_compose_samples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_samples.ui.theme.Jetpack_compose_samplesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MaterialTheme.colorScheme
             Jetpack_compose_samplesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "Row horizontalArrangement attribute")
+                        Row(
+                            verticalAlignment = Alignment.Top,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(0.2f)
+                                .background(MaterialTheme.colorScheme.surfaceVariant),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            Text(text = "Space")
+                            Text(text = "Evenly")
+                        }
+                        Row(
+                            verticalAlignment = Alignment.Top,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(0.2f)
+                                .background(MaterialTheme.colorScheme.surfaceVariant),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Text(text = "Space")
+                            Text(text = "Around")
+                        }
+                        Row(
+                            verticalAlignment = Alignment.Bottom,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(0.2f)
+                                .background(MaterialTheme.colorScheme.surfaceVariant),
+                            horizontalArrangement = Arrangement.spacedBy(24.dp)
+                        ) {
+                            Text(text = "Space")
+                            Text(text = "Around")
+                            Text(text = "horizontalArrangement = Arrangement.SpaceAround")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Jetpack_compose_samplesTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/jetpack_compose_samples/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetpack_compose_samples/ui/theme/Theme.kt
@@ -10,22 +10,48 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF006688),
+    onPrimary = Color(0xffffffff),
+    primaryContainer = Color(0xffbee8ff),
+    onPrimaryContainer = Color(0xff001e2b),
+    secondary = Color(0xFF4e616c),
+    onSecondary = Color(0xFFffffff),
+    secondaryContainer = Color(0xFFd1e6f3),
+    onSecondaryContainer = Color(0xFF091e28),
+    tertiary = Color(0xFF5e5a7c),
+    onTertiary = Color(0xffffffff),
+    tertiaryContainer = Color(0xffe5dfff),
+    onTertiaryContainer = Color(0xff1b1736),
+    error = Color(0xffba1b1b),
+    background = Color(0xfffbfcfe),
+    onBackground = Color(0xff191c1e),
+    surfaceVariant = Color(0xffdce3e9)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
+    primary = Color(0xFF006688),
+    onPrimary = Color(0xffffffff),
+    primaryContainer = Color(0xffbee8ff),
+    onPrimaryContainer = Color(0xff001e2b),
+    secondary = Color(0xFF4e616c),
+    onSecondary = Color(0xFFffffff),
+    secondaryContainer = Color(0xFFd1e6f3),
+    onSecondaryContainer = Color(0xFF091e28),
+    tertiary = Color(0xFF5e5a7c),
+    onTertiary = Color(0xffffffff),
+    tertiaryContainer = Color(0xffe5dfff),
+    onTertiaryContainer = Color(0xff1b1736),
+    error = Color(0xffba1b1b),
+    background = Color(0xfffbfcfe),
+    onBackground = Color(0xff191c1e),
+    surfaceVariant = Color(0xffdce3e9)
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
